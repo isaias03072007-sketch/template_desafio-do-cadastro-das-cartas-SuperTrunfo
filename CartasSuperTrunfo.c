42882022a95610ab9516a08ec495ac2ff5f8fd56
isaias03072007-sketch <261267030+isaias03072007-sketch@users.noreply.github.com>
2026-02-23 20:45:28 +0000

    anotações

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,32 @@
 #include <stdio.h>
 int main() {
 
-  char codigo[10];
-    char cidade[30];
-    char estado;
-    int habitantes;
-    int pontos;
-    float area, PIB;
-    int numero;
+    char codigo[10]; // Caracteres/strings. Declara uma variável de caractere   
+    char cidade[30]; // Caracteres/strings. Declara uma variável de caractere
+    char estado; // Caracteres/strings. Declara uma variável de caractere
+    int habitantes; //declara uma varíavel de numero inteiro chamada "habitantes".
+    int pontos; //declara uma váriavel de número inteiro chamada "Pontos" 
+    float area, PIB; // Casas decimais. Declara uma variável de ponto flutuante chamada "PIB".
+    int numero; //declara uma varíavel de numero inteiro chamada "número".
 
-    printf("Digite as Informações da sua carta:\n");
+    Printf("Desafio Super Trunfo!!") ; //imprime a informação que esta ente aspas.
 
-    printf("Carta (número): ");
-    scanf("%d", &numero);
+
+    printf("Digite as Informações da sua carta:\n"); //imprime a informação que esta ente aspas.
+
+    printf("Carta (número): "); // pede o número da carta
+    scanf("%d", &numero); // Lê o que o usuário digita e armazena no local certo com ajudado do %d.
+
+    /* o resto do código segue essa mesma lógica, o que muda é o indicador de dado "%". 
+    %d: Imprime um inteiro no formato decimal.
+    %i: equivalente a %d.
+    %f: Imprime uma variável de ponto flutuante no formato padrão.
+    %e: imprime uma variável de ponto flutuante na notção cientifica.
+    %c: imprime um único caractere.
+    %s: imprime uma cadeia de (string) de caracteres.
+
+    o \n é pra pular uma linha 
+*/
 
     printf("Digite uma letra de 'A' a 'H': ");
     scanf(" %c", &estado);
@@ -20,8 +34,10 @@ int main() {
     printf("Código da Carta (Ex. A01, B03): ");
     scanf(" %s", codigo);
 
-    printf("Nome da Cidade: ");
-    scanf(" %s", cidade);
+    printf("Nome da Cidade: "); 
+    scanf(" %s", cidade); /*eu não consegui fazer um espaço pro usuário escrever um nome de cidade
+    quebrado, tipo "são paulo", sempre que escrevo nomes assim o código para, mas funciona bem pra cidades 
+    com o nome interio tipo "Lábrea, Manaus..." eu não sei como resolver isso */
     printf("População: ");
     scanf(" %d", &habitantes);
 
@@ -35,6 +51,8 @@ int main() {
     scanf(" %d", &pontos);
 
     //----------------------------------------------------//
+
+    /*aqui eu apenas coloquei um caracter "1" na frente dos tipos de dados, e repeti o código */
 
     char codigo1[10];
     char cidade1[30];
@@ -56,7 +74,9 @@ int main() {
     scanf(" %s", codigo1);
 
     printf("Nome da Cidade: ");
-    scanf(" %s", cidade1); 
+    scanf(" %s", cidade1); /*eu não consegui fazer um espaço pro usuário escrever um nome de cidade
+    quebrado, tipo "são paulo", sempre que escrevo nomes assim o código para, mas funciona bem pra cidades 
+    com o nome interio tipo "Lábrea, Manaus..." eu não sei como resolver isso */
 
     printf("População: ");
     scanf(" %d", &habitantes1);
@@ -70,9 +90,15 @@ int main() {
     printf("Número de Pontos Turísticos: ");
     scanf(" %d", &pontos1);
 
-    printf("\n");
+    printf("\n"); //separa as coisas com uma linha imaginaria
 
-    printf("Dados da Carta 1: \n");
+    /*a sequencia de printf, abaixo imprime os dados da primeira carta, a atenção nos indicadores "%",
+    fora isso oq muda é a semantica, e o printf funciona assim:
+    printf("%formato1 %formato2 3formato3" variável1, variável2, variavl3, ...) ;
+    
+    usei a mesma lógica pra outra carta.*/
+
+    printf("Dados da Carta 1: \n");  
     printf("Carta: %d\n", numero);
     printf("Estado: %c\n", estado);
     printf("Codigo: %s\n", codigo);
@@ -82,7 +108,7 @@ int main() {
     printf("PIB: %.2f\n", PIB);
     printf("Pontos Turisticos: %d\n", pontos); 
 
-    printf("----------------------------------------------------------------------------------------------\n") ;
+    printf("-------------------------------------------------------------\n") ; //separa com uma linha de tracinhos
 
     printf("Dados da Carta 2: \n");
     printf("Carta: %d\n", numero1);
@@ -94,5 +120,5 @@ int main() {
     printf("PIB: %.2f\n", PIB1);
     printf("Pontos Turisticos: %d\n", pontos1); 
 
-return 0;
+return 0; // isso encerra a leitura do código 👀
 } 
